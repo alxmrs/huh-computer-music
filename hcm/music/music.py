0368210e366bb81c74223daded1bbc5c5e30fd23
Alex Rosengarten <alexrosengarten@gmail.com>
2017-10-28 16:43:23 -0700

    (Music)
    - Added doctests for frequency_map
    - Added types for frequency_map
    - refactored logic to be more efficient (no longer using interpolate).

--- hcm/music/music.py
+++ hcm/music/music.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-from scipy.interpolate import interp1d
-
 
 def scale_constructor(f0, key, num_octaves):
     """Makes a vector of frequencies to go over """
@@ -14,18 +12,37 @@ def scale_constructor(f0, key, num_octaves):
     return np.concatenate(all_octaves)
 
 
-def frequency_map(signal, scale):
+def frequency_map(signal: np.ndarray, scale: np.ndarray) -> np.ndarray:
     """Maps control voltage to position on scale.
 
     Takes a control voltage (CV) signal, whose range is on [-1.0, 1.0] and maps
     it to a corresponding position on the scale.
     Returns an array of
+
+    :param signal:
+    :param scale:
+    :return:
+
+    >>> signal = np.array([-1., -.5, .0, .5, 1.])
+    >>> frequency_map(signal, np.array([100, 200, 300, 400, 500]))
+    array([ 100.,  200.,  300.,  400.,  500.])
+    >>> frequency_map(signal, np.array([100, 200]))
+    array([ 100.,  100.,  100.,  100.,  200.])
+    >>> frequency_map(signal, np.array([100, 200, 300]))
+    array([ 100.,  100.,  200.,  200.,  300.])
+    >>> frequency_map(signal, np.array([100]))
+    array([ 100.,  100.,  100.,  100.,  100.])
+    >>> frequency_map(signal, \
+            np.array([100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]))
+    array([  100.,   300.,   500.,   700.,  1000.])
+    >>> frequency_map(signal, np.array([100, 200, 300, 400, 500, 600, 700]))
+    array([ 100.,  200.,  400.,  500.,  700.])
     """
-    interp = interp1d([-1, 1], [0, len(scale) - 1])
-    f = np.zeros(len(signal))
-    for n in range(0, len(signal)):
-        index = int(interp(signal[n]))
-        f[n] = scale[index]
+
+    bins = np.linspace(-1, 1, num=len(scale))
+    inds = np.digitize(signal, bins)
+    f = np.array([scale[i - 1] for i in inds], dtype=np.float64)
+
     return f
 
 
@@ -48,3 +65,8 @@ def tempo_to_frequency(tempo, note_duration):
     # calculate frequency
     frequency = (tempo / 60.) * 2 ** note  # [Hz]
     return frequency
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
