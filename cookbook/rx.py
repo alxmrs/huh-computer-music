031110505453c8c3e5eb083795d37f8d8d080d6d
Alex Rosengarten <alexrosengarten@gmail.com>
2017-12-31 15:48:03 -0800

    (Import Cleanup) - Moved `time` into `ts.py` - Cleaned up imports in rx py

--- cookbook/rx.py
+++ cookbook/rx.py
@@ -1,18 +1,14 @@
 
 import datetime as dt
-# TODO(Alex) Fix imports, they are messy
-from hcm.io import wav_write, append
-from hcm.music.notes import notes
-from hcm.music.keys import keys
-from hcm.music.music import scale_constructor, \
-    tempo_to_frequency, frequency_map
+import hcm.io
+import hcm.ts
+import hcm.music
 
-from hcm.signal.osc import sine, triangle
+# TODO(Alex) Figure out better module organization for this
+from hcm.music.music import scale_constructor, tempo_to_frequency, frequency_map
 
+import hcm.signal.osc as osc
 import hcm.signal.vc as vc
-
-
-from hcm.ts import sample_and_hold
 
 import hcm.signal
 
@@ -71,13 +67,13 @@ class WavFileOutput(rx.Observer):
         if self.cache is None:
             self.cache = value
         else:
-            self.cache = append(self.cache, value)
+            self.cache = hcm.io.append(self.cache, value)
 
     def on_completed(self):
         pass
 
     def write_wav(self):
-        wav_write(self.filename, SAMPLE_RATE, self.cache)
+        hcm.io.wav_write(self.filename, SAMPLE_RATE, self.cache)
 
     def on_error(self, error):
         print('An error occured, couldn\'t output wavefile', error)
@@ -91,7 +87,7 @@ def read_config_file(filepath: typing.Union[pathlib.Path, typing.AnyStr]) -> typ
 def demo_control(ts, func, *, hold, scale):
     return ts \
         .map(func) \
-        .map(lambda s: sample_and_hold(s, SAMPLE_RATE, hold)) \
+        .map(lambda s: hcm.ts.sample_and_hold(s, SAMPLE_RATE, hold)) \
         .map(lambda sp: frequency_map(sp, scale=scale))
 
 
@@ -99,8 +95,8 @@ if __name__ == '__main__':
 
     config = read_config_file('cookbook/params.json')
 
-    f0 = notes[config['start_note']]
-    key = keys[config['key']]
+    f0 = hcm.music.notes[config['start_note']]
+    key = hcm.music.keys[config['key']]
 
     scale = scale_constructor(f0, key, config['num_octaves'])
 
@@ -109,17 +105,17 @@ if __name__ == '__main__':
     ts = rx.Observable.interval(1000) \
         .map(lambda i: hcm.ts.time(i, i + 1, SAMPLE_RATE)) \
 
-    sine_control = demo_control(ts, lambda t: sine(t, f0),
+    sine_control = demo_control(ts, lambda t: osc.sine(t, f0),
                                 hold=hold, scale=scale)
 
-    triangle_control = demo_control(ts, lambda t: triangle(t, f0),
+    triangle_control = demo_control(ts, lambda t: osc.triangle(t, f0),
                                 hold=hold, scale=scale)
 
     vco_sine = rx.Observable.zip(ts, sine_control,
-                                 lambda t, c: vc.VCO(t, c, sine))
+                                 lambda t, c: vc.VCO(t, c, osc.sine))
 
     vco_triangle = rx.Observable.zip(ts, triangle_control,
-                                     lambda t, c: vc.VCO(t, c, triangle))
+                                     lambda t, c: vc.VCO(t, c, osc.triangle))
 
     vco_sine.subscribe(lambda x: print('sine: ', x.shape))
     vco_triangle.subscribe(lambda x: print('triangle: ', x.shape))
